3e712a5c645f0a26fbdd752643b98c553dba9dd4
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-12-08 02:42:40 -0300

    Add logic to show and close dialogue UI

--- Assets/Scripts/DialogueUI/DialogueUI.cs
+++ Assets/Scripts/DialogueUI/DialogueUI.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class DialogueUI : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text textLabel;
     [SerializeField] private DialogueObject testDialogue;
 
@@ -12,11 +13,13 @@ public class DialogueUI : MonoBehaviour
     private void Start()
     {
         typeWriterEffect = GetComponent<TypeWriterEffect>();
+        CloseDialogueBox();
         ShowDialogue(testDialogue);
     }
 
     public void ShowDialogue(DialogueObject dialogueObject)
     {
+        dialogueBox.SetActive(true);
         StartCoroutine(StepThroughDialogue(dialogueObject));
     }
 
@@ -27,5 +30,13 @@ public class DialogueUI : MonoBehaviour
             yield return typeWriterEffect.Run(dialogue, textLabel);
             yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
         }
+
+        CloseDialogueBox();
+    }
+
+    private void CloseDialogueBox()
+    {
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
     }
 }
