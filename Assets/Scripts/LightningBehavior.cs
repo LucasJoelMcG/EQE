adcc1f90f3cc3221c559032bc705ac7eca43ef19
LucasJoelMcG <93175256+LucasJoelMcG@users.noreply.github.com>
2022-12-22 22:00:15 -0300

    some quick fixes

--- Assets/Scripts/LightningBehavior.cs
+++ Assets/Scripts/LightningBehavior.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LightningBehavior : MonoBehaviour
 {
+    [SerializeField] CircleCollider2D myCollider;
     void Awake()
     {
         gameObject.SetActive(true);
@@ -12,7 +14,11 @@ public class LightningBehavior : MonoBehaviour
 
     IEnumerator selfDestruct()
     {
-        yield return new WaitForSeconds(1f);
-        Destroy(gameObject);    
+        yield return new WaitForSeconds(0.4f);
+        myCollider.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        myCollider.enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
     }
 }
